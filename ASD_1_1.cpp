af60645974192d5b381bb4ec02b5885eff1fad49
IronHamster <150971477+IronHamster@users.noreply.github.com>
2024-09-12 15:52:28 +0200

    Add files via upload

---
+++ ASD_1_1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+int main() {
+    int liczba, maksimum;
+
+    do {
+        std::cin >> maksimum;
+
+        if (maksimum == 0) {
+            return 0;
+        }
+
+    } while (maksimum < 1 || maksimum > 100);
+
+   
+    while (true) {
+        std::cin >> liczba;
+
+        if (liczba == 0) {
+            break;
+        }
+
+        if (liczba >= 1 && liczba <= 100 && liczba > maksimum) {
+            maksimum = liczba;
+        }
+    }
+
+    std::cout << maksimum << std::endl;
+
+    return 0;
+}
